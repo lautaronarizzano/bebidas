f992702c215c3007302d9858a177b2a8997fcb5d
Lautaro Narizzano <lautaronarizzano@gmail.com>
2022-11-06 19:34:18 -0300

    ya se a;ade al carrito

--- assets/js/index.js
+++ assets/js/index.js
@@ -88,7 +88,6 @@ const inputPrecio = document.querySelector('#precio')
 const subtotal = document.querySelector('#subtotal')
 
 
-
 async function printBottles() {
     traerProductos = await fetch("./assets/js/productos.json");
     let data = await traerProductos.json();
@@ -103,16 +102,35 @@ async function printBottles() {
         div.innerHTML = `
         <a name="brebaje"></a>
         <img src="${producto.img}">
-                <div id="info">
+                <div id="divInfo">
                     <h3 class="producto-nombre">${producto.nombre}</h3>
                     <h4 class="producto-precio">$${producto.precio}</h4>
-                    <input type="button" class="agregar" id="agregar${producto.id}" value="Agregar \n producto">
                 </div>
-        `;
+                `;
         bottles.appendChild(div);
+
+        const divInfo = document.querySelector("#divInfo")
+
+        const comprar = document.createElement('button')
+
+        comprar.className = "comprar"
+
+        comprar.className = "agregar"
+
+        comprar.innerText = "comprar"
+
+        div.append(comprar)
+
+        comprar.addEventListener("click", () => {
+            carro.push({
+                id: producto.id,
+                nombre: producto.nombre,
+                precio: producto.precio,
+                img: producto.img,
+            })
+            printCarrito()
+        })
     });
-
-
 
 }
 
@@ -166,8 +184,8 @@ printBottles()
 
 // }
 
+function printCarrito() {
 
-function carrito() {
     const div2 = document.createElement('div')
     div2.classList.add('carrito')
     div2.innerHTML = `
@@ -176,10 +194,42 @@ function carrito() {
     <tr>
     <th scope="col">#</th>
     <th scope="col">Producto</th>
+    <th scope="col">Cantidad</th>
     <th scope="col">Precio</th>
     <th scope="col">Subtotal</th>
     </tr>
 </thead>
+<tbody id="tbody">
+
+</tbody>
 
 `
+
+    let i = carro.length
+
+carro.forEach((producto) => {
+    let carritoContent = document.createElement("tr")
+    carritoContent.className = 'carrito-content'
+    carritoContent.innerHTML = `
+    <th scope="row">${i}</th>
+    <td>${producto.nombre}</td>
+    <td>${producto.precio}</td>
+    <td></td>
+    <td></td>
+    <td>
+    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+    </td>
+    `
+})
+
+const tbody = document.querySelector("tbody")
+
+
+
+tbody.append(carritoContent)
+
+master.append(div2)
+
 }
+
